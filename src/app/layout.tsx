40c1c96f6ce42bd314a15d0acf5c772b4d869da4
isabellas-portfolio <iype.i@northeastern.edu>
2024-10-22 22:16:47 -0400

    message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar></Navbar>
         {children}
         <Footer></Footer>
